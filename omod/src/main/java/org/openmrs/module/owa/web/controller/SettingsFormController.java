d4103f4a9b4aa0565b44beb11a7e054c809b8b2a
Namrata Nehete <n.nehete@gmail.com>
2015-11-25 23:32:23 -0500

    saving app settings

--- omod/src/main/java/org/openmrs/module/owa/web/controller/SettingsFormController.java
+++ omod/src/main/java/org/openmrs/module/owa/web/controller/SettingsFormController.java
@@ -1,91 +1,125 @@
 package org.openmrs.module.owa.web.controller;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.GlobalProperty;
+import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
-import org.openmrs.messagesource.MessageSourceService;
 import org.openmrs.web.WebConstants;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
-@RequestMapping("/module/owa")
+@RequestMapping("/module/owa/settings")
 public class SettingsFormController {
 	
 	private static final Log log = LogFactory.getLog(SettingsFormController.class);
 	
-	@Autowired
-	private MessageSourceService messageSourceService;
-	
-	private String message;
-	
-	@ModelAttribute("globalProps")
-	@RequestMapping(value = "/settings", method = RequestMethod.GET)
-	public List<GlobalProperty> globalProperties(ModelMap model) {
-		List<GlobalProperty> globalProps = Context.getAdministrationService().getGlobalPropertiesByPrefix("owa");
-		return globalProps;
+	@RequestMapping(method = RequestMethod.GET)
+	public void showForm() {
 	}
 	
-	@RequestMapping(value = "/settings", method = RequestMethod.POST)
-	public String handleSubmission(@RequestParam("PROP_VAL_NAME") String[] PROP_VAL_NAME,
-	        @RequestParam("PROP_NAME") String[] PROP_NAME, Object obj,
-	        @RequestParam("PROP_DESC_NAME") String[] PROP_DESC_NAME, HttpServletRequest request) throws IOException {
+	@RequestMapping(method = RequestMethod.POST)
+	public String handleSubmission(@ModelAttribute("globalPropertiesModel") GlobalPropertiesModel globalPropertiesModel,
+	        Errors errors, WebRequest request) {
 		
-		HttpSession session = request.getSession();
-		List<GlobalProperty> formBackingObject = extracted(obj);
-		Map<String, GlobalProperty> formBackingObjectMap = new HashMap<String, GlobalProperty>();
-		for (GlobalProperty prop : formBackingObject) {
-			formBackingObjectMap.put(prop.getProperty(), prop);
+		globalPropertiesModel.validate(globalPropertiesModel, errors);
+		if (errors.hasErrors())
+			return null; // show the form again
+			
+		AdministrationService administrationService = Context.getAdministrationService();
+		for (GlobalProperty p : globalPropertiesModel.getProperties()) {
+			administrationService.saveGlobalProperty(p);
 		}
 		
-		List<GlobalProperty> globalPropList = new ArrayList<GlobalProperty>();
-		String[] keys = PROP_NAME;
-		String[] values = PROP_VAL_NAME;
-		String[] descriptions = PROP_DESC_NAME;
-		
-		for (int x = 0; x < keys.length; x++) {
-			String key = keys[x];
-			String val = values[x];
-			String desc = descriptions[x];
-			GlobalProperty tmpGlobalProperty = formBackingObjectMap.get(key);
-			if (tmpGlobalProperty != null) {
-				tmpGlobalProperty.setPropertyValue(val);
-				tmpGlobalProperty.setDescription(desc);
-				globalPropList.add(tmpGlobalProperty);
-			}
-		}
-		
-		Context.getAdministrationService().saveGlobalProperties(globalPropList);
-		message = messageSourceService.getMessage("owa.saved");
-		session.setAttribute(WebConstants.OPENMRS_MSG_ATTR, message);
+		request.setAttribute(WebConstants.OPENMRS_MSG_ATTR, Context.getMessageSourceService().getMessage("general.saved"),
+		    WebRequest.SCOPE_SESSION);
 		return "redirect:settings.form";
 	}
 	
-	private List<GlobalProperty> extracted(Object obj) {
-		return (Context.getAdministrationService().getGlobalPropertiesByPrefix("owa"));
+	/**
+	 * @return
+	 */
+	@ModelAttribute("globalPropertiesModel")
+	public GlobalPropertiesModel getModel() {
+		List<GlobalProperty> editableProps = new ArrayList<>();
+		
+		editableProps.add(Context.getAdministrationService().getGlobalPropertyObject("owa.appBaseUrl"));
+		editableProps.add(Context.getAdministrationService().getGlobalPropertyObject("owa.appFolderPath"));
+		editableProps.add(Context.getAdministrationService().getGlobalPropertyObject("owa.appStoreUrl"));
+		
+		return new GlobalPropertiesModel(editableProps);
 	}
 	
-	protected Object formBackingObject(HttpServletRequest request) throws ServletException {
-		if (Context.isAuthenticated()) {
-			return Context.getAdministrationService().getGlobalPropertiesByPrefix("owa");
-		} else {
-			return new ArrayList<GlobalProperty>();
+	/**
+	 * Represents the model object for the form, which is typically used as a wrapper for the list
+	 * of global properties list so that spring can bind the properties of the objects in the list.
+	 * Also capable of validating itself
+	 */
+	public class GlobalPropertiesModel implements Validator {
+		
+		private List<GlobalProperty> properties;
+		
+		public GlobalPropertiesModel() {
+		}
+		
+		public GlobalPropertiesModel(List<GlobalProperty> properties) {
+			this.properties = properties;
+		}
+		
+		/**
+		 * @see org.springframework.validation.Validator#supports(java.lang.Class)
+		 */
+		@Override
+		public boolean supports(Class<?> clazz) {
+			return clazz.equals(getClass());
+		}
+		
+		/**
+		 * @see org.springframework.validation.Validator#validate(java.lang.Object,
+		 *      org.springframework.validation.Errors)
+		 */
+		@Override
+		public void validate(Object target, Errors errors) {
+		}
+		
+		/**
+		 * Returns the global property for the given propertyName
+		 * 
+		 * @param propertyName
+		 * @return
+		 */
+		public GlobalProperty getProperty(String propertyName) {
+			GlobalProperty prop = null;
+			for (GlobalProperty gp : getProperties()) {
+				if (gp.getProperty().equals(propertyName)) {
+					prop = gp;
+					break;
+				}
+			}
+			return prop;
+		}
+		
+		/**
+		 * @return
+		 */
+		public List<GlobalProperty> getProperties() {
+			return properties;
+		}
+		
+		/**
+		 * @param properties
+		 */
+		public void setProperties(List<GlobalProperty> properties) {
+			this.properties = properties;
 		}
 	}
 }
--- omod/src/main/webapp/settings.jsp
+++ omod/src/main/webapp/settings.jsp
@@ -1,50 +1,41 @@
 <%@ include file="/WEB-INF/template/include.jsp"%>
-<openmrs:require privilege="Manage Open Web Apps" otherwise="/login.htm"
-                 redirect="/module/owa/settings.form" />
+<openmrs:require privilege="Manage Open Web Apps" otherwise="/login.htm" redirect="/module/owa/settings.form" />
 <%@ include file="/WEB-INF/template/header.jsp"%>
 <%@ include file="template/localHeader.jsp"%>
-<%@ page import="org.openmrs.web.WebConstants" %>
-<%
-pageContext.setAttribute("message", session.getAttribute(WebConstants.OPENMRS_MSG_ATTR));
-pageContext.setAttribute("error", session.getAttribute(WebConstants.OPENMRS_ERROR_ATTR));
-session.removeAttribute(WebConstants.OPENMRS_MSG_ATTR);
-session.removeAttribute(WebConstants.OPENMRS_ERROR_ATTR);
-%>
-
 <openmrs:htmlInclude file="/moduleResources/owa/css/app.css"/>
 <openmrs:htmlInclude file="/moduleResources/owa/css/bootstrap.min.css"/>
 
-<c:if test="${not empty error}">
-    <div id="openmrs_error"><c:out value="${error}"></c:out></div>
-</c:if>
-
-<c:if test="${not empty message}">
-    <div id="openmrs_msg"><c:out value="${message}"></c:out></div>
-</c:if>
+<spring:hasBindErrors name="globalPropertiesModel">
+	<spring:message code="fix.error"/>
+</spring:hasBindErrors>
 
 <div id="settingsDiv" class="divTitle">
     <h1><span><spring:message code="owa.settings" />:</span></h1>
-    <form method="post">
+    <form:form method="post"  modelAttribute="globalPropertiesModel">
         <table width="100%" class="table table-striped table-hover table-condensed">
             <tbody style="cursor:pointer;">
-                <c:forEach var="globalProp" items="${globalProps}" varStatus="status">
+                <c:forEach var="prop" items="${globalPropertiesModel.properties}" varStatus="varStatus">
                     <tr>
                         <td>
-                            <div class="settingRow">
-                                <h4 class="settingName">
-                                    ${globalProp.property}
-                                </h4>
+                            <spring:nestedPath path="properties[${varStatus.index}]">
+                                <div class="settingRow">
+                                    <h4 class="settingName"><spring:message code="${prop.property}.label" /></h4>
+                                    <span class="settingValue">
+                                        <spring:bind path="propertyValue">
+                                            <c:set var="inputSize" value="50" scope="page" />
+                                            <input type="text" name="${status.expression}" value="${status.value}" size="${inputSize}">
+                                            <form:errors cssClass="error"/>
+                                        </spring:bind>
+                                    </span>
+                                    <span class="settingDescription">
+                                        ${prop.description}
+                                    </span>
+                                </div>
+                            </spring:nestedPath>
+                        </td>
+                    </tr>
+                </c:forEach>
 
-                                <span class="settingValue">
-                                    <input type="text" size="60" name="PROP_VAL_NAME" value="${globalProp.propertyValue}" size="30" maxlength="4000" />
-                                </span>
-                                <span class="settingDescription">${globalProp.description}
-                                </span>
-                            </div>
-
-                        </c:forEach>
-                    </td>
-                </tr>
             </tbody>
         </table>
         <div class="settingRow">
@@ -54,7 +45,7 @@ session.removeAttribute(WebConstants.OPENMRS_ERROR_ATTR);
                        onclick="javascript:window.location = '<openmrs:contextPath />/admin'" />
             </div>
         </div>
-    </form>
+    </form:form>
 </div>
 
 <%@ include file="/WEB-INF/template/footer.jsp"%>
