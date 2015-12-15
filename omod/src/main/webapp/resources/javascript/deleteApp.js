971d32842313710a1e79a64fa4a071841a8d906c
Namrata Nehete <n.nehete@gmail.com>
2015-12-15 17:20:56 -0500

    Correct the delete app functionality

--- omod/src/main/webapp/resources/javascript/deleteApp.js
+++ omod/src/main/webapp/resources/javascript/deleteApp.js
@@ -2,6 +2,6 @@ function deleteApp(appName) {
     var result = window.confirm('Do you want to delete this app?' + "\n\n" + appName);
     if (result)
     {
-        window.location = '/openmrs/module/owa/deleteApp.htm?appName=' + appName;
+        window.location = window.location.pathname.replace("manage.form","deleteApp.htm?appName=" + appName);
     }
 }
