51cf49971f5f5ab349ec07d55660b2a964225291
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-09-13 11:14:10 +0200

    OWA-8 Expand .owa from module's web resources'

--- omod/src/main/java/org/openmrs/module/owa/activator/OwaActivator.java
+++ omod/src/main/java/org/openmrs/module/owa/activator/OwaActivator.java
@@ -11,7 +11,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ModuleActivator;
-import org.openmrs.module.owa.AppActivities;
 import org.openmrs.module.owa.AppManager;
 import org.openmrs.module.owa.impl.DefaultAppManager;
 import org.openmrs.util.OpenmrsUtil;
@@ -26,15 +25,15 @@ import java.util.List;
  * This class contains the logic that is run every time this module is either started or stopped.
  */
 public class OwaActivator implements ModuleActivator, ServletContextAware {
-
+	
 	protected Log log = LogFactory.getLog(getClass());
-
+	
 	private static ServletContext servletContext;
-
+	
 	public void setServletContext(ServletContext context) {
 		this.servletContext = context;
 	}
-
+	
 	/**
 	 * @see ModuleActivator#willRefreshContext()
 	 */
@@ -42,34 +41,47 @@ public class OwaActivator implements ModuleActivator, ServletContextAware {
 	public void willRefreshContext() {
 		log.info("Refreshing OWA Module");
 	}
-
+	
 	/**
 	 * @see ModuleActivator#contextRefreshed()
 	 */
 	@Override
 	public void contextRefreshed() {
+		String owaStarted = Context.getAdministrationService().getGlobalProperty("owa.started");
+		if (!Boolean.valueOf(owaStarted)) {
+			//not sure if it ever happens, but it was originally implemented that way
+			return;
+		}
+		
 		/**
 		 * Used to move the files from omod webapp resources to owa folder when omod webapps contain
-		 * manifest.webapp
+		 * manifest.webapp or .owa files.
 		 */
 		String owaAppFolderPath = Context.getAdministrationService().getGlobalProperty(AppManager.KEY_APP_FOLDER_PATH);
-		if (null == owaAppFolderPath) {
+		if (owaAppFolderPath == null) {
 			owaAppFolderPath = OpenmrsUtil.getApplicationDataDirectory()
-					+ (OpenmrsUtil.getApplicationDataDirectory().endsWith(File.separator) ? "owa" : File.separator + "owa");
+			        + (OpenmrsUtil.getApplicationDataDirectory().endsWith(File.separator) ? "owa" : File.separator + "owa");
 			Context.getAdministrationService().setGlobalProperty(AppManager.KEY_APP_FOLDER_PATH, owaAppFolderPath);
 		}
-
-		String owaStarted = Context.getAdministrationService().getGlobalProperty("owa.started");
+		
+		File owaAppFolder = new File(owaAppFolderPath);
+		if (!owaAppFolder.exists()) {
+			owaAppFolder.mkdirs();
+		}
+		
 		String openmrsRootPath = servletContext.getRealPath("/");
-		String absPath = new File(openmrsRootPath, "WEB-INF" + File.separator + "view" + File.separator + "module").getAbsolutePath();
-
+		String absPath = new File(openmrsRootPath, "WEB-INF" + File.separator + "view" + File.separator + "module")
+		        .getAbsolutePath();
+		
 		File dir = new File(absPath);
 		try {
 			List<File> files = (List<File>) FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
 			for (File file : files) {
-				if (file.getCanonicalPath().contains("manifest.webapp") && owaStarted.equalsIgnoreCase("true")) {
+				if (file.getName().endsWith(".owa")) {
+					FileUtils.moveFile(file, new File(owaAppFolder, file.getName()));
+				} else if (file.getCanonicalPath().contains("manifest.webapp")) {
 					File source = file.getParentFile();
-					File dest = new File(owaAppFolderPath + File.separator + source.getName());
+					File dest = new File(owaAppFolder, source.getName());
 					FileUtils.moveDirectory(source, dest);
 					log.info("Moving file from: " + source + " to " + dest);
 				}
@@ -78,33 +90,33 @@ public class OwaActivator implements ModuleActivator, ServletContextAware {
 		catch (IOException e) {
 			log.error(e);
 		}
-
+		
 		/**
-		 * Check if there is any '.zip' file in OWA app directory and deploy them
-		 * this app manager is out of Spring context, so apps have to be reloaded here
+		 * Check if there is any '.zip' or '.owa' file in OWA app directory and deploy them this app
+		 * manager is out of Spring context, so apps have to be reloaded here
 		 */
 		AppManager appManager = new DefaultAppManager();
 		appManager.reloadApps();
-		File owaDir = new File(Context.getAdministrationService().getGlobalProperty(AppManager.KEY_APP_FOLDER_PATH));
-		File[] files = owaDir.listFiles();
+		File[] files = owaAppFolder.listFiles();
 		if (files != null) {
 			for (File file : files) {
-				if (file.getName().endsWith(".zip")) {
+				if (file.getName().endsWith(".zip") || file.getName().endsWith(".owa")) {
 					try {
-						log.info("Deploying OWA from: "+file.getName()+"...");
-						appManager.installApp(file, file.getName(), Context.getAdministrationService().getGlobalProperty(AppManager.KEY_APP_BASE_URL));
+						log.info("Deploying OWA from: " + file.getName() + "...");
+						appManager.installApp(file, file.getName(),
+						    Context.getAdministrationService().getGlobalProperty(AppManager.KEY_APP_BASE_URL));
 						file.delete();
 					}
 					catch (IOException e) {
-						log.error("Failed to deploy OWA from zip file: "+file.getName(), e);
+						log.error("Failed to deploy OWA from zip file: " + file.getName(), e);
 					}
 				}
 			}
 		}
-
+		
 		log.info("OWA Module refreshed");
 	}
-
+	
 	/**
 	 * @see ModuleActivator#willStart()
 	 */
@@ -112,7 +124,7 @@ public class OwaActivator implements ModuleActivator, ServletContextAware {
 	public void willStart() {
 		log.info("Starting OWA Module");
 	}
-
+	
 	/**
 	 * @see ModuleActivator#started()
 	 */
@@ -120,7 +132,7 @@ public class OwaActivator implements ModuleActivator, ServletContextAware {
 	public void started() {
 		log.info("OWA started");
 	}
-
+	
 	/**
 	 * @see ModuleActivator#willStop()
 	 */
@@ -128,7 +140,7 @@ public class OwaActivator implements ModuleActivator, ServletContextAware {
 	public void willStop() {
 		log.info("Stopping OWA Module");
 	}
-
+	
 	/**
 	 * @see ModuleActivator#stopped()
 	 */
@@ -136,5 +148,5 @@ public class OwaActivator implements ModuleActivator, ServletContextAware {
 	public void stopped() {
 		log.info("OWA Module stopped");
 	}
-
+	
 }
