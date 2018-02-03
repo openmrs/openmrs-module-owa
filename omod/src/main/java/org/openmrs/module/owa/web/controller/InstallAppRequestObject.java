11fa232185c3562ea6aa21062b152a134ad5b547
Male Michael <michael.male@andela.com>
2018-02-03 21:13:19 +0300

    Fix generated owa url after installation of OWA

--- omod/src/main/java/org/openmrs/module/owa/web/controller/InstallAppRequestObject.java
+++ omod/src/main/java/org/openmrs/module/owa/web/controller/InstallAppRequestObject.java
@@ -3,22 +3,15 @@ package org.openmrs.module.owa.web.controller;
 public class InstallAppRequestObject {
 
 	private String urlValue;
-	
-	private String fileName;
 
 	public InstallAppRequestObject() {
 	}
 
-	public InstallAppRequestObject(String urlValue, String fileName) {
+	public InstallAppRequestObject(String urlValue) {
 		this.urlValue = urlValue;
-		this.fileName = fileName;
 	}
 
 	public String getUrlValue() {
 		return urlValue;
-	}
-
-	public String getFileName() {
-		return fileName;
 	}
 }
